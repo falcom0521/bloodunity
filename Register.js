292d54900e11cd29e29942de36b1c49b7d04b7cf
Ajay <ajay.com>
2023-04-28 11:29:50 +0530

    completed

---
+++ Register.js
@@ -0,0 +1,45 @@
+const passwordInput = document.querySelector("#password_data");
+
+passwordInput.addEventListener("keyup", () => {
+    let password = passwordInput.value;
+    
+    var strongRegex = new RegExp("^(?=.{14,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+    var mediumRegex = new RegExp("^(?=.{10,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+
+    if(password.length===0)
+    {
+        document.querySelector(".safety").innerHTML = "Enter Password";
+    }
+    else
+    {
+        document.querySelector(".safety").innerHTML = '<span>Password Strength</span><span class="line" id="line1"></span><span class="line" id="line2"></span><span class="line" id="line3"></span>';
+    }
+
+    if (strongRegex.test(password)) 
+    {
+        document.querySelector("#line1").style.background = "green";
+        document.querySelector("#line2").style.background = "green";
+        document.querySelector("#line3").style.background = "green";
+    } 
+    else if (mediumRegex.test(password)) 
+    {
+        document.querySelector("#line1").style.background = "yellow";
+        document.querySelector("#line2").style.background = "yellow";
+    } 
+    else 
+    {
+        document.querySelector("#line1").style.background = "red";
+        
+    }
+})
+    function validatePhone() {
+      var phoneField = document.getElementById("phone");
+      var phonePattern = /999999999/; // Replace with your desired phone number pattern
+      if (!phonePattern.test(phoneField.value)) {
+        alert("Please enter a valid phone number in the format xxx-xxx-xxxx.");
+        phoneField.focus();
+        return false;
+      }
+      return true;
+    }
+ 
